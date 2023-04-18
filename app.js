b29f90ee5cc0fc3ef749edc8fb9ffed438c762cf
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-04-18 16:24:34 +0300

    Add the js the update css for style.css

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-const hum = document.querySelector(".hamburger-menu");
-const back = document.querySelector(".back");
-const works = document.querySelector(".works");
-const div = document.querySelector(".works");
-const about = document.querySelector(".about-section");
-const contact = document.querySelector(".about-contact")
-const menuNav = document.querySelector(".mainu-nav");
-const navItems = document.querySelectorAll(".nav-item");
-const cardwork2=document.querySelector(".cardwok-2");
-const cardwork1=document.querySelector(".cardwok-1");
-const toolbar=document.querySelector(".toolbar");
+const hum = document.querySelector('.hamburger-menu');
+const back = document.querySelector('.back');
+const works = document.querySelector('.works');
+const about = document.querySelector('.about-section');
+const contact = document.querySelector('.about-contact');
+const menuNav = document.querySelector('.mainu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+const toolbar = document.querySelector('.toolbar');
+const body = document.querySelector('body');
+const headline = document.querySelector('.headline');
 
-hum.addEventListener("click", function() {
+hum.addEventListener('click', () => {
   // toggle the "active" class on the hamburger button
-  hum.classList.toggle("active");
-  menuNav.classList.toggle("show");
-   back.classList.toggle('mobile');
-   toolbar.classList.toggle('mobile');
-   works.classList.toggle('mobile-hide');
-   contact.classList.toggle('mobile-hide');
-   about.classList.toggle('mobile-hide');
-   back.classList.toggle('blue');
+  hum.classList.toggle('active');
+  menuNav.classList.toggle('show');
+  back.classList.toggle('mobile');
+  toolbar.classList.toggle('mobile');
+  works.classList.toggle('mobile-hide');
+  contact.classList.toggle('mobile-hide');
+  about.classList.toggle('mobile-hide');
+  body.classList.toggle('blue');
+  headline.classList.toggle('head');
 });
 
 for (const list of navItems) {
-  list.addEventListener('click', function(event) {
+  list.addEventListener('click', () => {
     menuNav.classList.toggle('show'); // corrected class name
     back.classList.toggle('back');
-    console.log(menuNav)
   });
 }
