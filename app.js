b4e84f8b65d745258819fd05f794a12dd850a471
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-04-19 23:07:25 +0300

    Update the code to make it the same with figima design

--- app.js
+++ app.js
@@ -1,52 +1,20 @@
-const hum = document.querySelector('.hamburger-menu');
-const back = document.querySelector('.logop');
-const works = document.querySelector('.works');
-const about = document.querySelector('.about-section');
-const contact = document.querySelector('.about-contact');
-const menuNav = document.querySelector('.hidden');
-const navItems = document.querySelectorAll('.nav-item');
-const toolbar = document.querySelector('.toolbar');
-const body = document.querySelector('body');
-const headline = document.querySelector('.headline');
-const closeIcon = document.querySelector('.close i');
+const hamburgerMenu = document.querySelector('.menu-icon');
+const mobileNavItems = document.querySelectorAll('.mobile-nav-item');
+const mobileClose = document.querySelector('.mobile-menu-close');
 
-hum.addEventListener('click', () => {
-  hum.classList.toggle('in-active');
-  menuNav.classList.toggle('show');
-  back.classList.toggle('mobile');
-  toolbar.classList.toggle('mobile');
-  works.classList.toggle('mobile-hide');
-  contact.classList.toggle('mobile-hide');
-  about.classList.toggle('mobile-hide');
-  body.classList.toggle('blue');
-  headline.classList.toggle('head');
-  closeIcon.classList.toggle('show');
+hamburgerMenu.addEventListener('click', () => {
+  // show the mobile menu
+  document.querySelector('.mobile-menu').style.display = 'flex';
 });
 
-closeIcon.addEventListener('click', () => {
-  hum.classList.toggle('in-active');
-  menuNav.classList.toggle('show');
-  back.classList.toggle('mobile');
-  toolbar.classList.toggle('mobile');
-  works.classList.toggle('mobile-hide');
-  contact.classList.toggle('mobile-hide');
-  about.classList.toggle('mobile-hide');
-  body.classList.toggle('blue');
-  headline.classList.toggle('head');
-  closeIcon.classList.toggle('show');
+mobileClose.addEventListener('click', () => {
+  // show the mobile menu
+  document.querySelector('.mobile-menu').style.display = 'none';
 });
 
-navItems.forEach((e) => {
-  e.addEventListener('click', () => {
-    hum.classList.toggle('in-active');
-    menuNav.classList.toggle('show');
-    back.classList.toggle('mobile');
-    toolbar.classList.toggle('mobile');
-    works.classList.toggle('mobile-hide');
-    contact.classList.toggle('mobile-hide');
-    about.classList.toggle('mobile-hide');
-    body.classList.toggle('blue');
-    headline.classList.toggle('head');
-    closeIcon.classList.toggle('show');
+// close menu on click mobile-nav-item
+mobileNavItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    document.querySelector('.mobile-menu').style.display = 'none';
   });
 });
