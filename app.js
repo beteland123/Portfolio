0625d3c4b4902321d743f6722aa0643a01f5e948
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-04-19 15:44:21 +0300

    Update the app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const hum = document.querySelector('.hamburger-menu');
-const back = document.querySelector('.back');
+const back = document.querySelector('.logop');
 const works = document.querySelector('.works');
 const about = document.querySelector('.about-section');
 const contact = document.querySelector('.about-contact');
@@ -8,10 +8,10 @@ const navItems = document.querySelectorAll('.nav-item');
 const toolbar = document.querySelector('.toolbar');
 const body = document.querySelector('body');
 const headline = document.querySelector('.headline');
+const closeIcon = document.querySelector('.close i');
 
 hum.addEventListener('click', () => {
-  // toggle the "active" class on the hamburger button
-  hum.classList.toggle('active');
+  hum.classList.toggle('in-active');
   menuNav.classList.toggle('show');
   back.classList.toggle('mobile');
   toolbar.classList.toggle('mobile');
@@ -20,11 +20,25 @@ hum.addEventListener('click', () => {
   about.classList.toggle('mobile-hide');
   body.classList.toggle('blue');
   headline.classList.toggle('head');
+  closeIcon.classList.toggle('show');
+});
+
+closeIcon.addEventListener('click', () => {
+  hum.classList.toggle('in-active');
+  menuNav.classList.toggle('show');
+  back.classList.toggle('mobile');
+  toolbar.classList.toggle('mobile');
+  works.classList.toggle('mobile-hide');
+  contact.classList.toggle('mobile-hide');
+  about.classList.toggle('mobile-hide');
+  body.classList.toggle('blue');
+  headline.classList.toggle('head');
+  closeIcon.classList.toggle('show');
 });
 
 navItems.forEach((e) => {
   e.addEventListener('click', () => {
-    hum.classList.toggle('active');
+    hum.classList.toggle('in-active');
     menuNav.classList.toggle('show');
     back.classList.toggle('mobile');
     toolbar.classList.toggle('mobile');
@@ -33,5 +47,6 @@ navItems.forEach((e) => {
     about.classList.toggle('mobile-hide');
     body.classList.toggle('blue');
     headline.classList.toggle('head');
+    closeIcon.classList.toggle('show');
   });
 });
