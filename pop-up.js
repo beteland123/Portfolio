d00c06f8f4b775e2cd6dd210fdc5244a06e4bb54
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-04-21 03:19:35 +0300

    correct style linter error

--- pop-up.js
+++ pop-up.js
@@ -21,7 +21,7 @@ const works = [
         details:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
         featured_image: 'img/Snapshoot\ Portfolio2.png',
-        lang_list: [ 'html', 'Ruby on Rails','css','javascript'],
+        lang_list: [ 'html','css','javascript'],
         breadcrumbs: ['CANOPY','BACK END DEV' ,'2015'],
         source: 'https://github.com/beteland123',
         live_demo: 'https://github.com/beteland123',
@@ -33,7 +33,7 @@ const works = [
         details:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
         featured_image: 'img/Snapshoot\ Portfolio3.png',
-        lang_list: [ 'html', 'Ruby on Rails','css','javascript'],
+        lang_list: [ 'html','css','javascript'],
         breadcrumbs: ['FACEBOOK','FULL STACK Dev', '2015'],
         source: 'https://github.com/beteland123',
         live_demo: 'https://github.com/beteland123',
@@ -45,7 +45,7 @@ const works = [
         details:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
         featured_image: 'img/Snapshoot\ Portfolio.png',
-        lang_list: [ 'html', 'Ruby on Rails','css','javascript'],
+        lang_list: [ 'html','css','javascript'],
         breadcrumbs: ['CANOPY', 'BACK END DEV', '2015'],
         source: 'https://github.com/beteland123',
         live_demo: 'https://github.com/beteland123',
@@ -53,7 +53,7 @@ const works = [
   ];
   
 const workCardShouldReverse = (index) => {
-    if (index % 2 !== 0) {
+    if (index % 2 === 0) {
       return 'row-reverse card';
     }
     return 'card';
